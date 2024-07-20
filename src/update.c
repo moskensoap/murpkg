00915ab2f5dcccf0984a991c8918401325355a48
moskensoap <141073988+moskensoap@users.noreply.github.com>
2024-07-20 10:53:03 +0000

    fix bug: git pull failed

--- src/update.c
+++ src/update.c
@@ -2,8 +2,8 @@
 
 int git_pull_repo(const char *path)
 {
-    char command[2 * PATH_MAX + strlen(path)];
-    snprintf(command, sizeof(command), "cd %s && %s pull", path, git_PATH);
+    char command[4 * PATH_MAX + strlen(path)];
+    snprintf(command, sizeof(command), "cd %s && %s reset --hard HEAD && %s clean -fd && %s pull", path, git_PATH, git_PATH, git_PATH);
     if (system(command) != 0)
     {
         perror("system");
